3379362747e071510e676a622c064d99e99d1135
rocky ishimwe <rockyishimwe9@gmail.com>
2026-04-03 20:29:29 +0200

    added resumes jsons

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,33 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import ResumeCard from "~/components/ResumeCard";
+import { resumes } from "~/constants";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/public/images/bg-main.svg') bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track your applications & Resume Ratings</h1>
+        <h2>Review your submissions and check AI-powered feedback.</h2>
+      </div>
+    </section>
+    {resumes.length > 0 && (
+
+    <div className="resumes-section">
+       
+      {resumes.map((resume)=>(
+        <ResumeCard key={resume.id} resume={resume}/>
+      ))}
+    </div>
+    )}
+  </main>;
 }

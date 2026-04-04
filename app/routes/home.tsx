1aeb886dc0f0bda6e475d13da7acd248abd430df
rocky ishimwe <rockyishimwe9@gmail.com>
2026-04-04 18:47:49 +0200

    added fully working backend

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -15,19 +15,19 @@ export default function Home() {
   return <main className="bg-[url('/public/images/bg-main.svg') bg-cover">
     <Navbar />
     <section className="main-section">
-      <div className="page-heading">
+      <div className="page-heading py-16">
         <h1>Track your applications & Resume Ratings</h1>
         <h2>Review your submissions and check AI-powered feedback.</h2>
       </div>
-    </section>
     {resumes.length > 0 && (
-
-    <div className="resumes-section">
+      
+      <div className="resumes-section">
        
       {resumes.map((resume)=>(
         <ResumeCard key={resume.id} resume={resume}/>
       ))}
     </div>
     )}
+    </section>
   </main>;
 }

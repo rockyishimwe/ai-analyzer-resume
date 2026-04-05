40b62a1f6ed6bf289d2b5e9a8478e23cebc58f21
rocky ishimwe <rockyishimwe9@gmail.com>
2026-04-05 20:37:39 +0200

    added authentication of puter

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,9 @@ import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
 import ResumeCard from "~/components/ResumeCard";
 import { resumes } from "~/constants";
+import { useState,useEffect } from "react";
+import { useNavigate,useLocation } from "react-router";
+import { usePuterStore } from "~/lib/puter";
 
 
 export function meta({}: Route.MetaArgs) {
@@ -12,6 +15,11 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const {auth} = usePuterStore();
+    const navigate = useNavigate();
+    useEffect(()=>{
+        if(!auth.isAuthenticated) navigate('/auth?next=/');
+    },[auth.isAuthenticated])
   return <main className="bg-[url('/public/images/bg-main.svg') bg-cover">
     <Navbar />
     <section className="main-section">

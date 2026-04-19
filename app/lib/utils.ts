369d8cd823ad5d2d4e5f1bc4f39425aff3c86f24
rocky ishimwe <rockyishimwe9@gmail.com>
2026-04-19 10:49:55 +0200

    Resume Feedback components

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,3 +1,12 @@
+import type { ClassValue } from "clsx";
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+ 
+ export function cn(...inputs:ClassValue[]){
+  return twMerge(clsx(inputs))
+ }
+ 
  export const formatSize = (bytes: number): string => {
   if (bytes === 0) return '0 B';
   const k = 1024;
--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -3,7 +3,8 @@ import { Link, useParams,useNavigate } from "react-router"
 import { usePuterStore } from "~/lib/puter";
 import ScoreCircle from "~/components/ScoreCircle";
 import Summary from "~/components/Summary";
-
+import ATS from "~/components/ATS";
+ import Details from "~/components/Details";
 
 export const meta = () =>([
     {title: 'Resumind | Review'},
@@ -77,7 +78,7 @@ const resume = () => {
                     <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
                         <Summary feedback={feedback}/>
                         <ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips ||[] }/>
-                        <Details feedback={feedback} /> 
+                        <Details feedback={feedback} />
                     </div>
                 ):(
                     <img src="/images/resume-scan-2.gif" className="w-full" />

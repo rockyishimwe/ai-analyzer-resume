11969566a5dca4d3dbae6820f05ba7603f65e359
rocky ishimwe <rockyishimwe9@gmail.com>
2026-04-19 13:27:20 +0200

    feat: initial commit

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,20 +1,6 @@
-import type { ClassValue } from "clsx";
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
- 
- export function cn(...inputs:ClassValue[]){
+export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
- }
- 
- export const formatSize = (bytes: number): string => {
-  if (bytes === 0) return '0 B';
-  const k = 1024;
-  const sizes = ['B', 'KB', 'MB', 'GB'];
-  const i = Math.floor(Math.log(bytes) / Math.log(k));
-  return parseFloat((bytes / Math.pow(k, i)).toFixed(1)) + ' ' + sizes[i];
-};
-
-
-
-export const generateUUID = () =>crypto.randomUUID(); 
+}
